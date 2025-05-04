815f4a9124420e78ed4873e8345aaa549f3fb211
Flavio Barbosa <flavio.barbosa+UMGEMU@umusic.com>
2025-05-04 08:25:26 -0300

    initial commit

---
+++ src/main/java/com/neo/neogame/NeoGameApplication.java
@@ -0,0 +1,13 @@
+package com.neo.neogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/neo_game/NeoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.neo_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
